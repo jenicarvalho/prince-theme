399eb73758c6a00d15ebb4ce5b0744fa1193365c
Jeniffer Carvalho <jenicarvalho@live.com>
2016-09-11 19:08:48 -0300

    inclusao sharing box e get_sidebar

--- single.php
+++ single.php
@@ -49,6 +49,7 @@
           <div class="action-post">
             <ul>
               <li><a href="#">Compartilhar: </a></li>
+              <div class="addthis_sharing_toolbox"></div>
             </ul>
           </div>
 
@@ -59,122 +60,7 @@
       <!--/ end MAIN posts -->  
     </div>
 
-
-    <div class="col-md-5 hidden-sm hidden-xs pull-right">
-    <!-- SIDEBAR -->
-    <aside class="sidebar">
-      
-      <!-- CONTAINER SIDE-->
-      <div class="container-side">
-        
-        <!-- busca -->
-        <div class="busca-blog">
-          <input type="text" name="busca" class="busca-blog" placeholder="Search..">
-          <button><i class="fa fa-search" ></i></button>
-        </div>
-        <!--/ end busca -->
-
-        <!-- POPULAR POST-->
-        <div class="post-sidebar">
-          <h3 class="title-h3">POSTS POPULARES</h3>
-          <span class="border-fun"></span>
-
-          <ul class="posts-popular">
-            <!-- single post -->
-            <li>
-              <img src="img/postside.jpg">
-              <h4><a href="#">Totally Free Photos For Personal And Commercial Use</a></h4>
-              <span class="dia">10/08/2016</span>
-            </li>
-            <!--/ end single post -->
-
-            <!-- single post -->
-            <li>
-              <img src="img/postside.jpg">
-              <h4><a href="#">Totally Free Photos For Personal And Commercial Use</a></h4>
-              <span class="dia">10/08/2016</span>
-            </li>
-            <!--/ end single post -->
-
-            <!-- single post -->
-            <li>
-              <img src="img/postside.jpg">
-              <h4><a href="#">Totally Free Photos For Personal And Commercial Use</a></h4>
-              <span class="dia">10/08/2016</span>
-            </li>
-            <!--/ end single post -->
-
-            <!-- single post -->
-            <li>
-              <img src="img/postside.jpg">
-              <h4><a href="#">Totally Free Photos For Personal And Commercial Use</a></h4>
-              <span class="dia">10/08/2016</span>
-            </li>
-            <!--/ end single post -->
-            
-            <!-- single post -->
-            <li>
-              <img src="img/postside.jpg">
-              <h4><a href="#">Totally Free Photos For Personal And Commercial Use</a></h4>
-              <span class="dia">10/08/2016</span>
-            </li>
-            <!--/ end single post -->
-
-
-            <!-- single post -->
-            <li>
-              <img src="img/postside.jpg">
-              <h4><a href="#">Totally Free Photos For Personal And Commercial Use</a></h4>
-              <span class="dia">10/08/2016</span>
-            </li>
-            <!--/ end single post -->
-          </ul>
-
-        </div>
-        <!-- / end POPULAR POST-->
-
-        <!-- POST TAGS -->
-        <div class="posts-tags">
-          <h3 class="title-h3">TAGS</h3>
-          <span class="border-fun"></span>
-          
-          <ul class="tags">
-            <li><a href="#">Banco de dados</a></li>
-            <li><a href="#">JSON</a></li>
-            <li><a href="#">Fonts</a></li>
-            <li><a href="#">Temas</a></li>
-            <li><a href="#">Guias</a></li>
-            <li><a href="#">Front end</a></li>
-          </ul>
-        </div>
-        <!-- / end POST TAGS -->
-
-
-        <!-- POST CATEGORIES -->
-        <div class="posts-categories">
-          <h3 class="title-h3">CATEGORIAS</h3>
-          <span class="border-fun"></span>
-
-          <ul class="categories">
-            <li><a href="#">Design</a></li>
-            <li><a href="#">Desenvolvimento</a></li>
-            <li><a href="#">CMS</a></li>
-            <li><a href="#">HTML</a></li>
-            <li><a href="#">PHP</a></li>
-            <li><a href="#">JAVASCRIPT</a></li>
-            <li><a href="#">CSS3</a></li>
-            <li><a href="#">SEO</a></li>
-          </ul>
-        </div>
-        <!--/ end POST CATEGORIES -->
-
-      </div>
-      <!-- / end CONTAINER SIDE-->
-
-
-    </aside>
-    <!-- / end SIDEBAR -->
-    </div>
+    <?php get_sidebar(  ); ?>
   </div>
 </div>
 <!-- / end blog-main -->
