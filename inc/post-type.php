a9a80191e373c4fef644b401b806d1b3bed92697
Jeniffer Carvalho <jenicarvalho@live.com>
2016-09-13 15:37:42 -0300

    criação do post type slide

--- inc/post-type.php
+++ inc/post-type.php
@@ -128,3 +128,111 @@ function post_type_sobre() {
 
 add_action('init', 'post_type_sobre');
 
+
+
+
+
+
+
+// ********************************************* 
+//******************* POST TYPE SLIDE
+//*********************************************
+
+
+
+function post_type_slide() {
+
+    $nomeSingular = 'Slide';
+    $nomePlural = 'Slides';
+    $description = 'Slide da home';
+
+    $labels = array(
+        'name' => $nomePlural,
+        'name_singular' => $nomeSingular,
+        'add_new_item' => 'Adicionar novo ' . $nomeSingular,
+        'edit_item' => 'Editar ' . $nomeSingular
+
+    );
+
+     $supports = array(
+        'title',
+        'editor',
+        'thumbnail'
+    );
+
+    $args = array(
+            'labels' => $labels,
+            'public' => true,
+            'description' => $description,
+            'menu_icon' => 'dashicons-format-image',
+       		'supports' => $supports
+    );
+
+    register_post_type( 'slide', $args );
+}
+
+add_action('init', 'post_type_slide');
+
+
+
+//adiciona metabox
+
+function meta_slide() {
+	add_meta_box(
+		'link-agencia',
+		'Informações no slide',
+		'slide_metabox',
+		'slide',
+		'normal',
+		'default'
+	);
+}
+
+add_action('add_meta_boxes', 'meta_slide');
+
+function slide_metabox( $post ) { 
+	$slide_texto = get_post_meta( $post->ID ); ?>
+
+	<style>
+
+		.metabox-input {
+			height: 100%;
+			border: 1px solid #CCC;
+			border-left: none;
+			margin: 0.5em 0;
+			width: 100%
+		}
+
+	</style>
+
+		<div class="metabox-item">
+			<div class="input-addon-wrapper">
+				<input class="metabox-input" type="text" name="principal"
+				value="<?= $slide_texto['principal'][0]; ?>" placeholder="Título Principal">
+			</div>
+		</div>
+
+		<div class="metabox-item">
+			<div class="input-addon-wrapper">
+				<input  class="metabox-input" type="text" name="subtitulo"
+				value="<?= $slide_texto['subtitulo'][0]; ?>" placeholder="Subtitulo">
+			</div>
+		</div>
+<?php
+
+}
+
+function meta_info_slide( $post_id ) {
+	if( isset($_POST['subtitulo']) ) {
+		update_post_meta( $post_id, 'subtitulo', sanitize_text_field( $_POST['subtitulo'] ) );
+	}
+
+	if( isset($_POST['principal']) ) {
+		update_post_meta( $post_id, 'principal', sanitize_text_field( $_POST['principal'] ) );
+	}
+
+}
+
+add_action('save_post', 'meta_info_slide');
+
+
