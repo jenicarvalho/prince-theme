cc5e24ba6b7d6008b9a947a3f6de8125ee348c3f
Jeniffer Carvalho <jenicarvalho@live.com>
2016-10-27 22:04:58 -0200

    pagina 404 adicionada

---
+++ 404.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+  <!-- SLIDER -->
+  <section class="slider-single" style="background: url('<?php echo PXS_THEME_URL?>/img/slider/criacao-de-sites-404.jpg');">
+     <div class="blog-slide">
+        <div class="container">
+            <h2>ERRO 404! </h2>
+            <div class="single-data">
+              <h4>Oops, essa página não existe ou foi movida.</h4>
+          </div>
+
+        </div>
+    </div>
+  </section>
+  <!-- /end slider -->
+
+<?php get_footer(); ?>
